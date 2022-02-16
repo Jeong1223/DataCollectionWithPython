ad868a4c0a7d29ff69c119e80187f128f629f5f1
Jeong Eun Jang <71644016+Jeong1223@users.noreply.github.com>
2022-02-16 00:26:39 -0400

    Add files via upload

---
+++ Code_Challenge.py
@@ -0,0 +1,14 @@
+# Prime Number Finder
+
+ef prime_finder(n): 
+  primes = []
+  for i in range (2, n+1):
+      for j in range(2, i):
+          if i%j == 0:
+              break
+      else:
+          primes.append(i)
+  return primes
+
+ 
+print(prime_finder(11))
